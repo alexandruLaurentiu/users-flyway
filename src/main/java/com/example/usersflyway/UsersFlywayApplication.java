8d35e0220c85141b5e48517caa8d45d2cb6c3ed2
ALEXANDRU-LAURENTIU HODUT <alexandru.laurentiu.hodut@ro.ibm.com>
2019-05-15 17:17:48 +0300

    Init repo

---
+++ src/main/java/com/example/usersflyway/UsersFlywayApplication.java
@@ -0,0 +1,13 @@
+package com.example.usersflyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersFlywayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersFlywayApplication.class, args);
+	}
+
+}
